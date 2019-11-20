7c9d9b3b1a5ec9f467f2f0766a9f744db91187d2
solar-is <mrpe4kin96@gmail.com>
2019-11-20 23:57:39 +0300

    program logic improved

--- src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/service/ServiceFactory.java
@@ -1,134 +1,319 @@
 package com.efimchick.ifmo.web.jdbc.service;
 
+import com.efimchick.ifmo.web.jdbc.ConnectionSource;
 import com.efimchick.ifmo.web.jdbc.domain.Department;
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
 
-import java.util.Comparator;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ServiceFactory {
-    private ServiceDataAndUtils data = new ServiceDataAndUtils();
 
-    private List<Employee> getPageOfEmployeeList(Paging paging, List<Employee> list) {
-        int left = paging.itemPerPage * (paging.page - 1);
-        int right = paging.itemPerPage * paging.page;
-        return list.subList(left, Math.min(right, list.size()));
+    private ResultSet getResultSetOfExecute(String SQL) throws SQLException {
+        ConnectionSource connectionSource = ConnectionSource.instance();
+        Connection connection = connectionSource.createConnection();
+        return connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE).executeQuery(SQL);
     }
 
-    private List<Employee> getEmployeesByDepartment(Department department) {
-        return data.employeesWithoutChain.stream()
-                .filter(employee -> employee.getDepartment() != null)
-                .filter(employee -> employee.getDepartment().getId().equals(department.getId()))
-                .collect(Collectors.toList());
+    private List<Employee> getEmployeeListByResultSet(ResultSet res) throws SQLException {
+        res.beforeFirst();
+        List<Employee> ans = new ArrayList<>();
+        while (res.next()) {
+            ans.add(employeeRowMapper(res,true));
+        }
+        return ans;
     }
 
-    private List<Employee> getEmployeesByManager(Employee manager) {
-        return data.employeesWithoutChain.stream()
-                .filter(employee -> employee.getManager() != null)
-                .filter(employee -> employee.getManager().getId().equals(manager.getId()))
-                .collect(Collectors.toList());
-    }
-
-    private List<Employee> sortEmployeesByHire(List<Employee> employees) {
-        employees.sort(Comparator.comparing(Employee::getHired));
-        return employees;
-    }
-
-    private List<Employee> sortEmployeesByLastName(List<Employee> employees) {
-        employees.sort(Comparator.comparing(e -> e.getFullName().getLastName()));
-        return employees;
-    }
-
-    private List<Employee> sortEmployeesBySalary(List<Employee> employees) {
-        employees.sort(Comparator.comparing(Employee::getSalary));
-        return employees;
-    }
-
-    private List<Employee> sortEmployeesByDepartmentAndLastName(List<Employee> employees) {
-        employees.sort((e1, e2) -> {
-            if (e1.getDepartment() == null) {
-                return -1;
+    private Employee employeeRowMapper(ResultSet resultSet, boolean isFirstLevel) {
+        Employee cur = null;
+        try {
+            String managerId = resultSet.getString("MANAGER");
+            if (!isFirstLevel){
+                managerId = null;
             }
-            if (e2.getDepartment() == null) {
-                return 1;
-            }
-            int res = e1.getDepartment().getName().compareTo(e2.getDepartment().getName());
-            if (res != 0) {
-                return res;
-            }
-            return e1.getFullName().getLastName().compareTo(e2.getFullName().getLastName());
-        });
-        return employees;
+            String departmentId = resultSet.getString("DEPARTMENT");
+            cur = new Employee(
+                    new BigInteger(String.valueOf(resultSet.getInt("ID"))),
+                    new FullName(
+                            resultSet.getString("FIRSTNAME"),
+                            resultSet.getString("LASTNAME"),
+                            resultSet.getString("MIDDLENAME")
+                    ),
+                    Position.valueOf(resultSet.getString("POSITION")),
+                    LocalDate.parse(resultSet.getString("HIREDATE")),
+                    new BigDecimal(resultSet.getInt("SALARY")),
+                    managerId == null ? null : getEmployeeById(new BigInteger(managerId)),
+                    departmentId == null ? null : getDepartmentById(new BigInteger(departmentId))
+            );
+        } catch (SQLException ignored) {
+        }
+        return cur;
     }
+
+    private Employee employeeRowMapper(ResultSet resultSet) {
+        Employee cur = null;
+        try {
+            String managerId = resultSet.getString("MANAGER");
+            String departmentId = resultSet.getString("DEPARTMENT");
+            cur = new Employee(
+                    new BigInteger(String.valueOf(resultSet.getInt("ID"))),
+                    new FullName(
+                            resultSet.getString("FIRSTNAME"),
+                            resultSet.getString("LASTNAME"),
+                            resultSet.getString("MIDDLENAME")
+                    ),
+                    Position.valueOf(resultSet.getString("POSITION")),
+                    LocalDate.parse(resultSet.getString("HIREDATE")),
+                    new BigDecimal(resultSet.getInt("SALARY")),
+                    managerId == null ? null : getEmployeeByIdWithChain(new BigInteger(managerId)),
+                    departmentId == null ? null : getDepartmentById(new BigInteger(departmentId))
+            );
+        } catch (SQLException ignored) {
+        }
+        return cur;
+    }
+
+    private Employee getEmployeeByIdWithChain(BigInteger Id) {
+        try {
+            ResultSet res = getResultSetOfExecute("select * from employee where id = " + Id);
+            res.next();
+            return employeeRowMapper(res);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Employee getEmployeeById(BigInteger Id) {
+        try {
+            ResultSet res = getResultSetOfExecute("select * from employee where id = " + Id);
+            res.next();
+            return employeeRowMapper(res,false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Department getDepartmentById(BigInteger Id) {
+        try {
+            ResultSet res = getResultSetOfExecute("select * from department where id = " + Id);
+            return getDepartmentListByResultSet(res).get(0);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private Department departmentRowMapper(ResultSet res) {
+        Department cur = null;
+        try {
+            cur = new Department(
+                    new BigInteger(res.getString("ID")),
+                    res.getString("NAME"),
+                    res.getString("LOCATION")
+            );
+        } catch (SQLException ignored) {
+        }
+        return cur;
+    }
+
+    private List<Department> getDepartmentListByResultSet(ResultSet res) throws SQLException {
+        res.beforeFirst();
+        List<Department> ans = new ArrayList<>();
+        while (res.next()) {
+            ans.add(departmentRowMapper(res));
+        }
+        return ans;
+    }
+
 
     public EmployeeService employeeService() {
         return new EmployeeService() {
             @Override
             public List<Employee> getAllSortByHireDate(Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesByHire(data.employeesWithoutChain));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee order by hiredate" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getAllSortByLastname(Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesByLastName(data.employeesWithoutChain));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee order by lastname" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getAllSortBySalary(Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesBySalary(data.employeesWithoutChain));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee order by salary" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getAllSortByDepartmentNameAndLastname(Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesByDepartmentAndLastName(data.employeesWithoutChain));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee left join department on " +
+                                    " employee.department = department.id order by department.name,employee.lastname " +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getByDepartmentSortByHireDate(Department department, Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesByHire(getEmployeesByDepartment(department)));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee where department = "+department.getId()+"order by hiredate" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getByDepartmentSortBySalary(Department department, Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesBySalary(getEmployeesByDepartment(department)));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee where department = "+department.getId()+"order by salary" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getByDepartmentSortByLastname(Department department, Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesByLastName(getEmployeesByDepartment(department)));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee where department = "+department.getId()+"order by lastname" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getByManagerSortByLastname(Employee manager, Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesByLastName(getEmployeesByManager(manager)));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee where manager = "+manager.getId()+"order by lastname" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getByManagerSortByHireDate(Employee manager, Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesByHire(getEmployeesByManager(manager)));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee where manager = "+manager.getId()+"order by hiredate" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public List<Employee> getByManagerSortBySalary(Employee manager, Paging paging) {
-                return getPageOfEmployeeList(paging, sortEmployeesBySalary(getEmployeesByManager(manager)));
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee where manager = "+manager.getId()+"order by salary" +
+                                    " limit " + paging.itemPerPage +
+                                    " offset " + paging.itemPerPage * (paging.page - 1)
+                    );
+                    return getEmployeeListByResultSet(res);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
 
             @Override
             public Employee getWithDepartmentAndFullManagerChain(Employee employee) {
-                for(Employee e : data.employeesWithChain){
-                    if (e.getId().equals(employee.getId())){
-                        return e;
-                    }
+                try {
+                    ResultSet resultSet = getResultSetOfExecute("select * from employee where id = " + employee.getId());
+                    resultSet.next();
+                    return employeeRowMapper(resultSet);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
                 }
-                return null;
             }
 
             @Override
             public Employee getTopNthBySalaryByDepartment(int salaryRank, Department department) {
-                List<Employee> employees = getEmployeesByDepartment(department);
-                employees.sort((e1, e2) -> e2.getSalary().compareTo(e1.getSalary()));
-                return employees.get(salaryRank-1);
+                try {
+                    ResultSet res = getResultSetOfExecute(
+                            "select * from employee where department = "+department.getId()+"order by salary desc" +
+                                    " limit 1" +
+                                    " offset " + (salaryRank-1)
+                    );
+                    return getEmployeeListByResultSet(res).get(0);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
         };
     }
